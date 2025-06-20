bc74f4bacadbb21031d4644eb723a265c39ea966
xyrct301 <xyrct301@outlook.com>
2025-06-20 10:49:42 +0800

    logo

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -49,7 +49,7 @@ export default defineThemeConfig({
    * @see https://theme-plume.vuejs.press/config/basic/#profile
    */
   profile: {
-    avatar: '/ncwumc/index/logo.png',
+    avatar: '/index/logo.png',
     name: 'NCWUMC',
     description: '画梦方块社',
     // circle: true,
