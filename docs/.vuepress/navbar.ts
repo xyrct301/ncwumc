0125d2ebf2a8d2bb219dc9e75be58a3e38d67114
xyrct301 <xyrct301@outlook.com>
2026-02-05 20:51:06 +0800

    修复抖音官号链接问题

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -20,7 +20,7 @@ export default defineNavbarConfig([
     { text: '社团成员', link: '/notes/list/README.md' },
     { text: 'QQ群', link: 'https://qun.qq.com/universal-share/share?ac=1&authKey=FForLmXqIB1Mf3TRRIE5Rno3kASuCbTeA2FaOfDtIR4zs7E0rE81O60co7ZqvXdO&busi_data=eyJncm91cENvZGUiOiI3OTQwNjE4MTAiLCJ0b2tlbiI6Ii9Id0IxajVPby9sM0R5WjZGS0hBUkJuVzYvbUVsNThWNmlIdTFNUjVxRDF6UXNya2cvMTBBdmxpaDZxRjVqZVIiLCJ1aW4iOiIyMTU5OTU0MTg5In0%3D&data=mXBE6JncAFCIMPeN6bBNwPb6KEGAkv8Tr_2ht5iQmGBhZSfBsnpn10B8bFkWxvNPpe4GYvs7kx7ZFH3zZwoLPQ&svctype=4&tempid=h5_group_info' },
     { text: 'B站官号', link: 'https://space.bilibili.com/3493277240133634?spm_id_from=333.337.0.0' },
-    { text: '抖音官号', link: '/notes/list/README.md' }
+    { text: '抖音官号', link: 'https://v.douyin.com/sWyNPPnaQPQ/' }
   ]
   },
 ])
